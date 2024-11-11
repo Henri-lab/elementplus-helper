d1c9ab7c325b4abdaaf2faf83f5b39ad429182f5
henriFox.W <62045411+Henri-lab@users.noreply.github.com>
2024-11-11 23:25:15 +0800

    ''

--- src/components/Table/example/TargetBasicInfo.vue
+++ src/components/Table/example/TargetBasicInfo.vue
@@ -1,67 +1,91 @@
 <template>
-    <div class="table-target-basic-info">
-        <ToolBarOfTable></ToolBarOfTable>
+  <div class="table-target-basic-info">
+    <ToolBarOfTable></ToolBarOfTable>
 
-        <EnhancedTable :columns="columns" :initialData="paginatedData" :isAddBtn="false" :isDeleteSelected="false">
-        </EnhancedTable>
-        <br>
+    <EnhancedTable
+      :columns="columns"
+      :initialData="paginatedData"
+      :isAddBtn="false"
+      :isDeleteSelected="false"
+    >
+      <template #operation="scope">
+        <el-button @click="handleCustomBtn(scope.row)" size="small">
+          关联图片
+        </el-button>
+        <el-button @click="handleCustomBtn(scope.row)" size="small">
+          导出
+        </el-button>
+      </template>
+    </EnhancedTable>
+    <br />
 
-        <!-- 分页器 -->
-        <EnhancedPagination :total="totalItems" :currentPage="currentPage" :pageSize="pageSize"
-            @update:currentPage="onPageChange" @update:pageSize="onPageSizeChange" />
-    </div>
+    <!-- 分页器 -->
+    <EnhancedPagination
+      :total="totalItems"
+      :currentPage="currentPage"
+      :pageSize="pageSize"
+      @update:currentPage="onPageChange"
+      @update:pageSize="onPageSizeChange"
+    />
+  </div>
 </template>
-
-
 
 <script lang="ts">
 import { computed, defineComponent, ref } from 'vue';
 //@ts-ignore
-import ToolBarOfTable from '@/components/ToolBar/OfTable.vue'
+import ToolBarOfTable from '@/components/ToolBar/OfTable.vue';
 import EnhancedTable from '../index.vue';
 //@ts-ignore
 import { targetBasicInfoData } from '@/mock/table_test';
 //@ts-ignore
-import config_columns from '../config/targetBasicInfo'
+import config_columns from '../config/targetBasicInfo';
 import EnhancedPagination from '../../Pagination/index.vue';
 
-
 export default defineComponent({
-    components: { EnhancedTable, EnhancedPagination, ToolBarOfTable },
-    setup() {
-        const columns = config_columns;
-        const data = targetBasicInfoData;// 模拟数据
+  components: { EnhancedTable, EnhancedPagination, ToolBarOfTable },
+  setup() {
+    const columns = config_columns;
+    const data = targetBasicInfoData; // 模拟数据
 
-        const handleCustomBtn = (row: any) => {
-            console.log('custom button clicked:', row);
-        };
+    const handleCustomBtn = (row: any) => {
+      console.log('custom button clicked:', row);
+    };
 
-        const totalItems = ref(data.length || 500); // 总条数
-        const currentPage = ref(1); // 当前页码
-        const pageSize = ref(10); // 每页条数
+    const totalItems = ref(data.length || 500); // 总条数
+    const currentPage = ref(1); // 当前页码
+    const pageSize = ref(10); // 每页条数
 
-        const allData = data;
+    const allData = data;
 
-        // 计算当前页的数据
-        const paginatedData = computed(() => {
-            const start = (currentPage.value - 1) * pageSize.value;
-            const end = start + pageSize.value;
-            return allData.slice(start, end);
-        });
+    // 计算当前页的数据
+    const paginatedData = computed(() => {
+      const start = (currentPage.value - 1) * pageSize.value;
+      const end = start + pageSize.value;
+      return allData.slice(start, end);
+    });
 
-        // 页码变化
-        const onPageChange = (page: number) => {
-            currentPage.value = page;
-        };
+    // 页码变化
+    const onPageChange = (page: number) => {
+      currentPage.value = page;
+    };
 
-        // 每页条数变化
-        const onPageSizeChange = (size: number) => {
-            pageSize.value = size;
-            currentPage.value = 1; // 重置到第一页
-        };
+    // 每页条数变化
+    const onPageSizeChange = (size: number) => {
+      pageSize.value = size;
+      currentPage.value = 1; // 重置到第一页
+    };
 
-
-        return { columns, data, handleCustomBtn, totalItems, currentPage, pageSize, paginatedData, onPageChange, onPageSizeChange };
-    }
+    return {
+      columns,
+      data,
+      handleCustomBtn,
+      totalItems,
+      currentPage,
+      pageSize,
+      paginatedData,
+      onPageChange,
+      onPageSizeChange,
+    };
+  },
 });
 </script>
